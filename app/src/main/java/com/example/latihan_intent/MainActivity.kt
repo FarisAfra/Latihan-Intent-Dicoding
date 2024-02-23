5865f4ae6dff469bb75901b6d852dd3f769e5279
Faris Afra <diafan74@gmail.com>
2024-02-23 13:17:43 +0700

    Intent With Object Data (Parcelable)
    Latihan Mengirim Data dengan Parcelable

--- app/src/main/java/com/example/latihan_intent/MainActivity.kt
+++ app/src/main/java/com/example/latihan_intent/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnMoveWithObject:Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -30,6 +33,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Faris Afra Mu'taz")
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 20)
                 startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "Faris Afra Mu'taz",
+                    20,
+                    "farisafra@apps.ipb.ac.id",
+                    "Bogor"
+                )
+
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
             }
         }
     }
